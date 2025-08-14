f904288ddc5390435ec1a4d9a2ccdd8c29993d38
Pluggito <pluggitoshfl5050@gmail.com>
2025-08-14 18:36:34 +0100

    Working on the email list

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,5 @@
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Card, CardDescription, CardTitle } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 
 
@@ -8,6 +10,47 @@ export default function HomePage() {
     <div>
       <h1>My Email List</h1>
       <Separator className="my-4"/>
+      <p>Here you can see all your emails</p>
+
+      {/*----------Emails--------*/}
+      <div className="grid col-auto">
+        <Card>
+          <CardTitle className="flex items-center gap-4">
+            <Avatar>
+              <AvatarFallback>
+                TN
+              </AvatarFallback>
+            </Avatar>
+            <CardDescription>
+              <p>Email Body</p>
+            </CardDescription>
+          </CardTitle>
+        </Card>
+        <Card>
+          <CardTitle>
+            <h2>Email Subject</h2>
+            <CardDescription>
+              <p>Email Body</p>
+            </CardDescription>
+          </CardTitle>
+        </Card>
+        <Card>
+          <CardTitle>
+            <h2>Email Subject</h2>
+            <CardDescription>
+              <p>Email Body</p>
+            </CardDescription>
+          </CardTitle>
+        </Card>
+        <Card>
+          <CardTitle>
+            <h2>Email Subject</h2>
+            <CardDescription>
+              <p>Email Body</p>
+            </CardDescription>
+          </CardTitle>
+        </Card>        
+      </div>
     </div>
   </div>;
 }
